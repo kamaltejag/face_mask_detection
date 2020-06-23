4ff244c4eb62c5a350cbfb76ea4da327382305de
kamaltejag <kamaltejag@gmail.com>
2020-06-23 14:36:33 +0530

    Added Files

---
+++ app.py
@@ -0,0 +1,17 @@
+from views.AuthView import AuthView
+from views.DetectionView import DetectionView
+
+
+class MyApp:
+    def run(self):
+        av = AuthView()
+        av.transfer_control = self.detection
+        av.load()
+
+    def detection(self):
+        dv = DetectionView()
+        dv.load()
+
+
+app = MyApp()
+app.run()
